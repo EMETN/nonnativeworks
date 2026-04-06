9bddb61374298ea057c59f575705cd247b4f401f
EMETN <268669549+eemetnoo@users.noreply.github.com>
2026-04-06 16:00:01 +0000

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import sentry from '@sentry/astro';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: node({ mode: 'standalone' }),
+    adapter: netlify(),
     integrations: [
         preact(),
         sentry({
