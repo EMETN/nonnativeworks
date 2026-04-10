ee2645a44671b0c44b41209a8031c665e7a5d0c2
Stella <charm-amnesty-kite@duck.com>
2026-04-10 13:03:57 +0300

    Fix domain in allowed domains

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -102,7 +102,7 @@ for domain in \
     "www.accenture.com" \
     "posti.wd3.myworkdayjobs.com" \
     "sok.wd502.myworkdayjobs.com" \
-    "ag.wd3.myworkdayjobs.com/Airbus" \
+    "ag.wd3.myworkdayjobs.com" \
     "equinor.wd3.myworkdayjobs.com" \
     "storaenso.wd502.myworkdayjobs.com" \
     "kone.wd3.myworkdayjobs.com" \
