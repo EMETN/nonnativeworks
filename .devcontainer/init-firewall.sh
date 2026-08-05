07948810047469f78c8193a8f4f66044c84dd5d0
EMETN <268669549+eemetnoo@users.noreply.github.com>
2026-08-05 14:59:45 +0300

    Add Yousician to firewall

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -132,6 +132,7 @@ for domain in \
     "nitor.com" \
     "alpha-sense.com" \
     "rovio.com" \
+    "yousician.com" \
     "careers.microsoft.com" \
     "apply.careers.microsoft.com" \
     "s-pankki.fi" \
